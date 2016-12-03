aca681f518e439f28b2a591c40663a085f274f04
Mike Diehl <mdiehl@diehlnet.com>
2016-12-03 11:04:48 -0500

    Moved device.pm functions from utils.pm

--- Manager.pl
+++ Manager.pl
@@ -17,7 +17,7 @@ utils::get_config();
 
 require "./features/core.pm";
 
-utils::read_devices();
+device::read_devices();
 
 print "Registered $device_count devices.\n";
 
@@ -44,72 +44,5 @@ sub	process {
 		&{$functions{core}{$h->{command}}}($h);
 		return;
 	}
-}
-
-
-__DATA__
-sub	dispatch {
-	my($topic, $message) = @_;
-	my($f);
-
-	if ($topic eq "") { return 0; }
-
-	$f = $topic;
-
-	$f =~ s/$prefix//e;
-	$f =~ s/\//::/g;
-	$f =~ s/^:://;
-
-	($item) = split(/\//, $f);
-
-	print "Received message: ($topic = $message): ";
-
-	($p,$board,$pin) = split(/\//, $topic);
-	print "X: $p $board $mac{$board} $pin $pinnumber{$pin}";
-
-	$pinnumber = $pinnumber{$pin};
-
-	# /PREFIX/DEVICE/PINNUMBER
-	if (defined $pin{$mac{$board}}{$pinnumber}) {
-		print "Pin function\n";
-		return;
-	}
-
-	# /PREFIX/DEVICE/FUNCTION
-	if (defined $mac{$board}) {
-		print "Device function\n";
-		return;
-	}
-
-	# PREFIX/DEVICENAME/  @@@ Why do we ignore this?
-	if ($device{$f}{name} ne "") {
-		print "Ignoring message for ($f) ($device{$f}{$name})\n";
-		return;
-	}
-
-	# @@@ Why do we ignore this?
-	if ($pin{$f}{name} ne "") {
-		print "Ignoring message for ($f) ($pin{$f}{$name})\n";
-		return;
-	}
-
-	# /PREFIX/raw/FUNCTION
-	if ($functions{$f} ne "") {
-		&{$f}($topic, $message);
-		print "Processed\n";
-		return;
-	}
-
-	# /PREFIX/raw/GARBAGE
-	if ($functions{$f} eq "") {
-		if ($topic =~ m/${prefix}\/raw/) {
-			print "Ignoring raw message\n";
-			return;
-		}
-	}
-
-	print "Extraneous\n";
-
-	return 0;
 }
 
