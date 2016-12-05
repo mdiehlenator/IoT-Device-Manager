d77d7caf5df2d47126fa74dbdd9fb46bcf815194
Mike Diehl <mdiehl@diehlnet.com>
2016-12-05 17:47:51 -0500

    Begin processing platform-specific functions

--- device_definitions/esp8266.pm
+++ device_definitions/esp8266.pm
@@ -2,6 +2,26 @@ package esp8266;
 
 BEGIN {
 	print "Device definition for esp8266 has been loaded.\n";
+
+	$main::functions{esp8266}{version} = esp8266::version;
+
+}
+
+sub	version {
+	my($h) = @_;
+
+	print "esp8266::version() called.\n";
+
+	if ($h->{from} eq "manager") {
+		print "This came from the manager, so we are skipping it.\n";
+		return;
+	}
+
+	my $device = device::find_device($h->{to});
+
+	$to = $device->{mac};
+
+	mqtt::publish("$main::config{mqtt_prefix}/$to/manager/version", "a");
 }
 
 return 1;
