83e8554fa97c4ffc3fa21afb59c293083a1af636
Mike Diehl <mdiehl@diehlnet.com>
2017-01-13 17:28:24 -0500

    Captured some code I didn\'t want to lose.

--- features/core.pm
+++ features/core.pm
@@ -30,3 +30,38 @@ sub	status {
 }
 
 return 1;
+
+__DATA__
+
+sub	boot {
+	my($topic, $device) = @_;
+	my($name);
+
+	print "Looking for name for $device and found $main::device{$device}{name}\n";
+	$name = $main::device{$device}{name};
+
+	foreach $pin (keys %{$main::pin{$device}}) {
+
+		if ($pin,$main::pin{$device}{$pin}{mode} eq "digitalinput") {
+			protocol::pinmode($device, $pin, "digitalinput");
+		}
+
+		if ($pin,$main::pin{$device}{$pin}{mode} eq "digitaloutput") {
+			protocol::pinmode($device, $pin, "digitaloutput");
+			protocol::digitalwrite($device, $pin, $main::pin{$device}{$pin}{initial});
+		}
+
+		if ($pin,$main::pin{$device}{$pin}{mode} eq "analoginput") {
+			protocol::pinmode($device, $pin, "analoginput");
+		}
+
+		if ($pin,$main::pin{$device}{$pin}{mode} eq "analogoutput") {
+			protocol::pinmode($device, $pin, "analogoutput");
+			protocol::analogwrite($device, $pin, $main::pin{$device}{$pin}{initial});
+		}
+	}
+
+	print "Boot: $topic, $device($name)\n";
+}
+
+return 1;
