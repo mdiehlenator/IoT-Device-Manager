e3914bbab8386aed39013d567ed4c77f9361ae33
Mike Diehl <mdiehl@diehlnet.com>
2017-01-13 17:06:00 -0500

    Removed raw.pm for now

--- Manager.pl
+++ Manager.pl
@@ -6,7 +6,6 @@ use FileHandle;
 
 require "./modules/utils.pm";
 require "./modules/mqtt.pm";
-require "./modules/raw.pm";
 require "./modules/device.pm";
 
 STDOUT->autoflush;
@@ -46,8 +45,15 @@ sub	process {
 	if ($from_device->{id} eq $h->{from}) { $from_id = 1;}
 	if ($to_device->{id} eq $h->{to}) { $to_id = 1;}
 
-	if (($from_id == 1) && !($h->{to} ne "manager")) { protocol_error($h); }	# No one but the manager should be receiving messages from devices directly.
-	if (($to_id ==1) && $h->{from} ne "managaer") { protocol_error($h); }		# No one but the manager should be sending messages to devices directly.
+	if (($h->{from} eq "manager") && ($to_id == 1)) { return; }			# This is a message from the manager to a device... we sent it, so we ignore it.
+
+	if (($from_id == 1) && ($h->{to} eq "manager")) { 
+		protocol_error($h, "Warn: Message came from device-id and sent to manager."); 
+	}	
+
+	if (($to_id ==1) && $h->{from} ne "managaer") { 
+		protocol_error($h, "Error: Someone sent a message directly to a device."); # No one but the manager should be sending messages to devices directly.
+	}
 
 	# Then, let's check the platform-specific functions.
 	if (($h->{to} eq "manager") and (device::find_device($h->{from}) ne "")) {
@@ -64,8 +70,14 @@ sub	process {
 }
 
 sub	protocol_error {
-	my($h) = @_;
+	my($h, $msg) = @_;
 
-	print "Protocol Error\n";
+	print "Protocol: ";
+
+	foreach $i (sort keys %$h) {
+		print "$i->$h->{$i} ";
+	}
+
+	print "($msg)\n";
 }
 
