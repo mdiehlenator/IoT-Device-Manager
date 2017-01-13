6c3252cc44bd1f7627b3364843a77f82985d5610
Mike Diehl <mdiehl@diehlnet.com>
2017-01-13 17:14:49 -0500

    Devined core::pong() and core::status() to avoid bogus proto errors.

--- features/core.pm
+++ features/core.pm
@@ -3,6 +3,8 @@ package core;
 BEGIN {
 	$main::functions{core}{boot} = core::boot;
 	$main::functions{core}{startup} = core::startup;
+	$main::functions{core}{pong} = core::pong;
+	$main::functions{core}{status} = core::status;
 
 	print "Core loaded.\n";
 }
@@ -14,6 +16,16 @@ sub	boot {
 
 sub	startup{
 	print "Core::startup() called\n";
+	return;
+}
+
+sub	pong{
+	print "Core::pong() called\n";
+	return;
+}
+
+sub	status {
+	print "Core::status() called\n";
 	return;
 }
 
